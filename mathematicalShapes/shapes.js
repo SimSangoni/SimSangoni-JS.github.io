4bf6054a89f66d5421abd75864e94419f10f78de
SimSangoni <118910298+SimSangoni@users.noreply.github.com>
2023-04-26 13:36:38 +0200

    statements

--- mathematicalShapes/shapes.js
+++ mathematicalShapes/shapes.js
@@ -8,3 +8,11 @@ const side_3 = 7;
 area = (1/4)*Math.sqrt(4*(side_2**2)*(side_3**2)-
 (side_2**2+side_3**2-side_1**2)**2);
 console.log(`The area of the triangle is ${area}`)
+
+const radius = 4;
+const pi = Math.PI;
+circumference = 2*pi*radius;
+surfaceArea = pi*radius**2;
+console.log(`The circumference of the circle is ${circumference}`)
+console.log(`The surface area of the circle is ${surfaceArea}`)
+
